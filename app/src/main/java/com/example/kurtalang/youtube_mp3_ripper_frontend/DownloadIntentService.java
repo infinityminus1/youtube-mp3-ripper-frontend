2089ce9640ef316e55f877c0505d143946b04e50
Kurt Lang <kurtalang@gmail.com>
2019-01-02 11:48:13 -0600

    Add DownloadManager, Intents for download, and volley. Volley handles making the HEAD call to the server first in order to get the video title. Then calls the DownloadManager to download the file with correct metadata.

--- app/src/main/java/com/example/kurtalang/youtube_mp3_ripper_frontend/DownloadIntentService.java
+++ app/src/main/java/com/example/kurtalang/youtube_mp3_ripper_frontend/DownloadIntentService.java
@@ -3,10 +3,13 @@ package com.example.kurtalang.youtube_mp3_ripper_frontend;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -15,7 +18,7 @@ import java.net.URL;
 public class DownloadIntentService extends IntentService {
 
     private static final String TAG = DownloadIntentService.class.getSimpleName();
-    private static final String URL_BASE = "http://54.215.234.18/?url=";
+    private static final String URL_BASE = "http://54.193.90.124/?url=";
 
     public static final String PENDING_RESULT_EXTRA = "pending_result";
     public static final String URL_EXTRA = "url";
@@ -38,6 +41,7 @@ public class DownloadIntentService extends IntentService {
     protected void onHandleIntent(@Nullable Intent intent) {
         URL url;
         HttpURLConnection urlConnection;
+        String filename;
 
         PendingIntent reply = intent.getParcelableExtra(PENDING_RESULT_EXTRA);
         String url_string = URL_BASE + intent.getStringExtra(URL_EXTRA);
@@ -60,37 +64,58 @@ public class DownloadIntentService extends IntentService {
             try {
                 Log.d(TAG, String.format("About to open connection to %s", url_string));
                 urlConnection = (HttpURLConnection) url.openConnection();
-            }
-            catch (java.io.IOException e) {
-                String msg = String.format("DownloadIntentService() openConnection() IOException ", e);
+                String contentDispositionHeaderField = urlConnection.getHeaderField("Content-Disposition");
+                String[] splitHeader = contentDispositionHeaderField.split("filename=");
+                filename = splitHeader[1];
+            } catch (java.io.IOException e) {
+                String msg = String.format("DownloadIntentService() openConnection() IOException %s", e);
                 Log.d(TAG, msg);
                 reply.send(CONNECTION_ERROR_CODE);
                 return;
-            } catch (Exception exc) {
-                // could do better by treating the different sax/xml exceptions individually
+            } catch (Exception e) {
+                String msg = String.format("Exception %s", e);
+                Log.d(TAG, msg);
                 reply.send(ERROR_CODE);
                 return;
             }
 
             // Get the result of the input stream if available and close the url connection
             try {
-                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-            }
-            catch (java.io.IOException e) {
-                String msg = String.format("makeYoutubeRipApiCall() getInputStream() IOException ", e);
+                Log.d(TAG, "About to call BufferedInputStream");
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                File downloadFile = new File(Environment.getExternalStorageDirectory().getPath() + "/" + filename);
+                FileOutputStream os = new FileOutputStream(downloadFile);
+                byte buffer[] = new byte[1024];
+                int byteCount;
+                while ((byteCount = is.read(buffer)) != -1) {
+                    os.write(buffer, 0, byteCount);
+                }
+                os.close();
+                is.close();
+
+                String filePath = downloadFile.getPath();
+                Log.d(TAG, String.format("filePath is: %s", filePath));
+//                bundle.putString("filePath", filePath);
+                reply.send(RESULT_CODE);
+            } catch (java.io.IOException e) {
+                String msg = String.format("IOException %s", e);
                 Log.d(TAG, msg);
                 reply.send(INPUT_STREAM_ERROR_CODE);
-            } catch (Exception exc) {
-                // could do better by treating the different sax/xml exceptions individually
+            } catch (Exception e) {
+                String msg = String.format("Exception %s", e);
+                Log.d(TAG, msg);
                 reply.send(ERROR_CODE);
-            }
-            finally {
+            } finally {
+                Log.d(TAG, "About to disconect from the url");
                 urlConnection.disconnect();
                 reply.send(RESULT_CODE);
             }
+
 
         } catch (PendingIntent.CanceledException exc) {
             Log.i(TAG, "reply cancelled", exc);
         }
     }
+
+
 }
--- app/src/main/java/com/example/kurtalang/youtube_mp3_ripper_frontend/MainActivity.java
+++ app/src/main/java/com/example/kurtalang/youtube_mp3_ripper_frontend/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.kurtalang.youtube_mp3_ripper_frontend;
 
+import android.app.DownloadManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +14,15 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+import com.android.volley.*;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int MP3_DOWNLOAD_REQUEST_CODE = 1000;
+    private final static String BACKEND_URL_BASE = "http://54.215.234.18/?url=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +66,27 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        final String TAG = "MainActivity.onActivityResult";
+        if (requestCode == MP3_DOWNLOAD_REQUEST_CODE) {
+            switch (resultCode) {
+                case DownloadIntentService.INVALID_URL_CODE:
+                    Log.d(TAG, "onActivityResult() received INVALID_URL_CODE");
+                    break;
+                case DownloadIntentService.ERROR_CODE:
+                    Log.d(TAG, "onActivityResult() received ERROR_CODE");
+                    break;
+                case DownloadIntentService.RESULT_CODE:
+                    Log.d(TAG, "onActivityResult() received RESULT_CODE");
+                    break;
+            }
+            Log.d(TAG, String.format("onActivityResult(): Data from intent is: %s", data));
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     public void handleContentDeliveredByIntent() {
         // Get intent, action and MIME type
         Intent intent = getIntent();
@@ -85,15 +117,98 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, msg);
                 return;
             }
-
-            makeYoutubeRipApiCall(sharedText);
-        }
-        else {
+            get_youtube_title_with_volley(sharedText);
+//            downloadData(sharedText);
+//            createIntentForApiCall(sharedText);
+        } else {
             Log.d(TAG, "handleSendText() received NO shared text");
         }
     }
 
-    public void makeYoutubeRipApiCall(String url) {
-        return;
+    public void createIntentForApiCall(String url) {
+        PendingIntent pendingResult = createPendingResult(MP3_DOWNLOAD_REQUEST_CODE, new Intent(), 0);
+        Intent intent = new Intent(getApplicationContext(), DownloadIntentService.class);
+
+        intent.putExtra(DownloadIntentService.URL_EXTRA, url);
+        intent.putExtra(DownloadIntentService.PENDING_RESULT_EXTRA, pendingResult);
+
+        startService(intent);
+    }
+
+    private long downloadData(String youtube_url, String youtube_title) {
+
+        String full_url = BACKEND_URL_BASE + youtube_url;
+        Uri uri = Uri.parse(full_url);
+
+        // Create request for android download manager
+        DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+//        request.allowScanningByMediaScanner();
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED); //Notify client once download is completed!
+
+//        downloadManager.enqueue(request);
+        Toast.makeText(getApplicationContext(), "Downloading File", //To notify the Client that the file is being downloaded
+                Toast.LENGTH_LONG).show();
+
+        //Setting title of request
+        request.setTitle(youtube_title);
+
+        //Setting description of request
+//        request.setDescription("Android Data download using DownloadManager.");
+
+        //Set the local destination for the downloaded file to a path
+        //within the application's external files directory
+        request.setDestinationInExternalFilesDir(MainActivity.this, Environment.DIRECTORY_DOWNLOADS, youtube_title);
+
+        //Enqueue download and save into referenceId
+        return downloadManager.enqueue(request);
+    }
+
+    public void get_youtube_title_with_volley(final String youtube_url) {
+
+        final String TAG = "get_youtube_title_with_volley.handleSendText";
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = BACKEND_URL_BASE + youtube_url;
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.HEAD, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // TODO: find "youtube_title" header value and return here.
+                        Log.d(TAG,"Response is: " + response.substring(0, 500));
+
+                        downloadData(youtube_url,"PLACE_TITLE_HERE");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG,String.format("That didn't work! %s", error));
+                    }
+                }
+        );
+        stringRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 5000000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
